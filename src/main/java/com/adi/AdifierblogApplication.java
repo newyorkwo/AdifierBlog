7769b26767ef80a9e16dd52c8f4462c1412bc355
Steven Wu <stephexx@yahoo.com.tw>
2019-04-23 10:27:55 +0800

    Create New Adifier Blog

---
+++ src/main/java/com/adi/AdifierblogApplication.java
@@ -0,0 +1,13 @@
+package com.adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdifierblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdifierblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adi/AdifierblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdifierblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
