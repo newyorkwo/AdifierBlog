503875f1baf0cdac346d65c1ecad3473be1f949d
newyorkwo <stephexx@gmail.com>
2019-05-09 18:35:22 +0800

    new Blog

--- src/main/java/com/adi/web/IndexController.java
+++ src/main/java/com/adi/web/IndexController.java
@@ -3,6 +3,7 @@ package com.adi.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @description: create index controller
@@ -15,10 +16,16 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         //int i=9/0;
-        String blog=null;
-        if(blog==null){
-            throw new NotFoundException("博客找不到");
-        }
+//        String blog=null;
+//        if(blog==null){
+//            throw new NotFoundException("博客找不到");
+//        }
+        //System.out.println("-------Index page------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "adifierBlog";
     }
 }
