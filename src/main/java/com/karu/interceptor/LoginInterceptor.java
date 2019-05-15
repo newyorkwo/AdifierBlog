4514276b4fa147bb6e7b0e086157effe60a9b3f9
newyorkwo <stephexx@gmail.com>
2019-05-15 16:33:00 +0800

    new Blog

---
+++ src/main/java/com/karu/interceptor/LoginInterceptor.java
@@ -0,0 +1,19 @@
+package com.karu.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @description: create interceptor
+ * @author: StevenWu
+ * @create: 2019-05-15 16:24
+ **/
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return super.preHandle(request, response, handler);
+    }
+}
