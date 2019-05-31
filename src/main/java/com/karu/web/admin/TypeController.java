0f5b0b2e6c320ff17495bdf8e2b7cf6864c99081
newyorkwo <stephexx@gmail.com>
2019-05-31 16:27:43 +0800

    new Blog

--- src/main/java/com/karu/web/admin/TypeController.java
+++ src/main/java/com/karu/web/admin/TypeController.java
@@ -8,10 +8,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 /**
  * @description: create Type Controller
@@ -33,13 +36,19 @@ public class TypeController {
     }
 
     @GetMapping("/types/input")
-    public String input(){
+    public String input(Model model){
+        model.addAttribute("type", new Type());
         return "admin/types-input";
     }
 
     @PostMapping("/types")
-    public String post(Type type, RedirectAttributes attributes){
+    public String post(@Valid Type type, BindingResult result, RedirectAttributes attributes){
+        if (result.hasErrors()) {
+            return "admin/types-input";
+        }
+
         Type t=typeService.saveType(type);
+
         if (t==null){
             attributes.addFlashAttribute("message","操作失敗");
         }else {
