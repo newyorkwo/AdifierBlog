7e05fd6c311488bdb6a6ff42fa41272124973ddd
newyorkwo <stephexx@gmail.com>
2019-06-05 11:04:34 +0800

    new Blog

--- src/main/java/com/karu/dao/BlogRepository.java
+++ src/main/java/com/karu/dao/BlogRepository.java
@@ -2,7 +2,8 @@ package com.karu.dao;
 
 import com.karu.domain.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BlogRepository extends JpaRepository<Blog, Long> {
+public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificationExecutor<Blog> {
 
 }
--- src/main/java/com/karu/service/BlogServiceImpl.java
+++ src/main/java/com/karu/service/BlogServiceImpl.java
@@ -1,8 +1,21 @@
 package com.karu.service;
 
+import com.karu.dao.BlogRepository;
 import com.karu.domain.Blog;
+import com.karu.web.NotFoundException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 描述:
@@ -11,31 +24,48 @@ import org.springframework.data.domain.Pageable;
  * @author StevenWu
  * @create 2019-06-04-22:53
  */
+@Service
 public class BlogServiceImpl implements BlogService{
 
+    @Autowired
+    private BlogRepository blogRepository;
 
     @Override
     public Blog getBlog(Long id) {
-        return null;
+
+        return blogRepository.findById(id).orElseThrow(NotFoundException::new);
     }
 
     @Override
     public Page<Blog> listBlog(Pageable pageable, Blog blog) {
-        return null;
+
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                List<Predicate> predicates=new ArrayList<>();
+                if (!"".equals(blog.getTitle()) && blog.getTitle() != null) {
+                    predicates.add(cb.like(root.<String>get("title"), "%"+blog.getTitle()+"%"));
+                }
+                return null;
+            }
+        }, pageable);
     }
 
     @Override
     public Blog saveBlog(Blog blog) {
-        return null;
+
+        return blogRepository.save(blog);
     }
 
     @Override
     public Blog updateBlog(Long id, Blog blog) {
-        return null;
+        Blog b=blogRepository.findById(id).orElseThrow(NotFoundException::new);
+        BeanUtils.copyProperties(b,blog);
+        return blogRepository.save(b);
     }
 
     @Override
     public void deleteBlog(Long id) {
-
+        blogRepository.deleteById(id);
     }
 }
