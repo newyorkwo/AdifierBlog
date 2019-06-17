7e37dcde83d3eaed0790be228798ef3ab3723b76
Steven Wu <stephexx@yahoo.com.tw>
2019-06-17 16:11:33 +0800

    change blog name

--- src/main/java/com/karu/web/admin/BlogController.java
+++ src/main/java/com/karu/web/admin/BlogController.java
@@ -2,6 +2,7 @@ package com.karu.web.admin;
 
 import com.karu.domain.Blog;
 import com.karu.service.BlogService;
+import com.karu.service.TagService;
 import com.karu.service.TypeService;
 import com.karu.vo.BlogQuery;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,18 +28,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class BlogController {
 
+    private static final String INPUT="admin/blogs-input";
+    private static final String LIST="admin/blogs";
+    private static final String REDIRECT_LIST="redirect:/admin/blogs";
     @Autowired
     private BlogService blogService;
-
     @Autowired
     private TypeService typeService;
+    @Autowired
+    private TagService tagService;
 
     @GetMapping("/blogs")
     public String blogs(@PageableDefault(size = 2, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
                         BlogQuery blog, Model model){
         model.addAttribute("types", typeService.listType());
         model.addAttribute("page", blogService.listBlog(pageable, blog));
-        return "admin/blogs";
+        return LIST;
     }
 
     @PostMapping("/blogs/search")
@@ -46,6 +51,18 @@ public class BlogController {
                          BlogQuery blog, Model model){
         model.addAttribute("page", blogService.listBlog(pageable, blog));
         return "admin/blogs :: blogList";
+    }
+
+    @GetMapping("/blogs/input")
+    public String input(Model model){
+        setTypeAndTag(model);
+        model.addAttribute("blog", new Blog());
+        return INPUT;
+    }
+
+    private void setTypeAndTag(Model model){
+        model.addAttribute("types",typeService.listType());
+        model.addAttribute("tags", tagService.listTag());
     }
 
 }
